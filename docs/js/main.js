1378896f3f6336cc71ce343472da5382b04bdd5e
EvgeniyaRo <htstrah@mail.ru>
2018-01-31 00:07:48 +0300

    final

--- docs/js/main.js
+++ docs/js/main.js
@@ -19,15 +19,34 @@ $(document).ready(function() {
 	    
 	});
 
-	// При изменении размера окна, в большую сторону, если меню было скрыто, показываем его.
+	// При изменении размера окна, в большую сторону, если меню было скрыто, показываем его
+	// И у кнопки убираем модификатор --active
 	$(window).resize(function(){
 	    var w = $(window).width();
 	    if(w > 992) {
 	        menu.removeAttr('style');
+	        pull.removeClass('navigation__toggle-button--active');
+	    } else {
+
 	    }
 	});
 
+	// Скрываем меню при клике на него на смартфоне и планцете
+	// По клику на ссылку в меню запускаем ф-ю fnstart();
+	$('nav.navigation a').on("click", function(){
+		fnstart();
+	});
+	
 
+// В ф-ии fnstart(); проверяем - если меню открыто (проверяем по наличию класса --active у кнопки pull)
+	// тогда убираем класс модификатор --active у кнопки pull
+	// и сворачиваем/скрываем меню 
+	function fnstart(){	
+		if ( $("#navigation__toggle").hasClass("navigation__toggle-button--active")  ) {
+   			pull.toggleClass('navigation__toggle-button--active');
+			menu.slideToggle();
+		}
+	};
 // Слайдер
 	$("#top-slider").owlCarousel({
 		singleItem: true,
@@ -35,6 +54,13 @@ $(document).ready(function() {
 		theme: "top-slider-theme",
 		navigationText: ["", ""]
 	});
+
+
+//slide2id - плавная прокрутка по ссылкам внутри страницы
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	    highlightSelector:"nav a"
+	});
+
 
 	
 });
